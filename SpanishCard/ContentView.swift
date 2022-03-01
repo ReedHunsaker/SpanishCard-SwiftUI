d317000d293171ea9fc2879d755ff8fd77438f42
Reed Hunsaker <reed.hunsaker@gmail.com>
2022-02-28 21:41:05 -0700

    Initial Commit

---
+++ SpanishCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpanishCard
+//
+//  Created by Reed hunsaker on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpanishCard/SpanishCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpanishCardApp.swift
+//  SpanishCard
+//
+//  Created by Reed hunsaker on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SpanishCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
